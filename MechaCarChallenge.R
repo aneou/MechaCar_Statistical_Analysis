74ac949fbd3306ffa7e7726ebe526579d05b7917
aneou <48192407+aneou@users.noreply.github.com>
2022-04-20 14:11:24 -0700

    Deliverable 1

---
+++ MechaCarChallenge.R
@@ -0,0 +1,7 @@
+library(dplyr)
+
+mpg_df<- read.csv("MechaCar_mpg.csv")
+
+lm(mpg~vehicle_length + vehicle_weight+spoiler_angle+ground_clearance+AWD, data = mpg_df)
+
+summary(lm(mpg~vehicle_length + vehicle_weight+spoiler_angle+ground_clearance+AWD, data = mpg_df))
