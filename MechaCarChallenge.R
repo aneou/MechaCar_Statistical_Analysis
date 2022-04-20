98b1b367b02c9b3fbbc6d68a3b6cdff818a1f31b
aneou <48192407+aneou@users.noreply.github.com>
2022-04-20 16:43:43 -0700

    Deliverable 3

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -11,3 +11,11 @@ coil_df <- read.csv("Suspension_Coil.csv")
 total_summary <- coil_df %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI))
 
 lot_summary <- coil_df %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI))
+
+t.test(coil_df["PSI"],mu=1500)
+
+t.test(coil_df[coil_df$Manufacturing_Lot == "Lot1","PSI"],mu=1500)
+
+t.test(coil_df[coil_df$Manufacturing_Lot == "Lot2","PSI"],mu=1500)
+
+t.test(coil_df[coil_df$Manufacturing_Lot == "Lot3","PSI"],mu=1500)
