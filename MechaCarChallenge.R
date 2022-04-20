762e9ea6b433e3a499cbf2a14722f086e6f7a9b6
aneou <48192407+aneou@users.noreply.github.com>
2022-04-20 16:26:06 -0700

    Deliverable 2

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -5,3 +5,9 @@ mpg_df<- read.csv("MechaCar_mpg.csv")
 lm(mpg~vehicle_length + vehicle_weight+spoiler_angle+ground_clearance+AWD, data = mpg_df)
 
 summary(lm(mpg~vehicle_length + vehicle_weight+spoiler_angle+ground_clearance+AWD, data = mpg_df))
+
+coil_df <- read.csv("Suspension_Coil.csv")
+
+total_summary <- coil_df %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI))
+
+lot_summary <- coil_df %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI))
